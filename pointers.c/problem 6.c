be666fa394c9763244448d39526c854f3a313a51
Shuvronila <shuvronilabhattacharjee@gmail.com>
2026-09-24 23:57:03 +0600

    solve of problem 6

---
+++ pointers.c/problem 6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+  int myNumbers[4] = {25, 50, 75, 100};
+  *myNumbers = 13;
+  *(myNumbers +1) = 17;
+  printf("%d\n", *myNumbers);
+  printf("%d\n", *(myNumbers + 1));  
+  
+  return 0;
+}

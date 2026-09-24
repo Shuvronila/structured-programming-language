b8d53567a1956f989e145157c03f4b623656a853
Shuvronila <shuvronilabhattacharjee@gmail.com>
2026-09-25 00:02:51 +0600

    solve of problem 10

---
+++ pointers.c/problem 10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int myNumbers[2] = {1, 2};
+  char letters[] = "Hi";
+
+  int *pi = myNumbers;   
+  char *pc = letters;    
+
+  printf("%p\n", (void*)pi);
+  printf("%p\n", (void*)(pi + 1)); 
+  printf("%p\n", (void*)(pi + 2)); 
+
+  printf("%p\n", (void*)pc);
+  printf("%p\n", (void*)(pc + 1)); 
+  return 0;
+}
